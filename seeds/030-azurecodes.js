62f31f2fa5c4f2d7f5bfddde7756d2b596019356
Hao Luo <howlowck@gmail.com>
2018-01-29 13:06:10 -0600

    changed migration to map azurecodes to students

--- seeds/030-azurecodes.js
+++ seeds/030-azurecodes.js
@@ -4,47 +4,47 @@ exports.seed = (knex, Promise) =>
         {
           code: 'abc',
           expires_on: '2018-03-01',
-          project_id: 1,
+          student_id: 1,
           hackathon_id: 1
         }, {
           code: 'def',
           expires_on: '2018-03-01',
-          project_id: 2,
+          student_id: 2,
           hackathon_id: 1
         }, {
           code: 'ghi',
           expires_on: '2018-03-01',
-          project_id: 3,
+          student_id: 3,
           hackathon_id: 1
         }, {
           code: 'jkl',
           expires_on: '2018-03-01',
-          project_id: 4,
+          student_id: 4,
           hackathon_id: 1
         }, {
           code: 'mno',
           expires_on: '2018-03-01',
-          project_id: null,
+          student_id: null,
           hackathon_id: 1
         }, {
           code: 'pqr',
           expires_on: '2018-03-01',
-          project_id: null,
+          student_id: null,
           hackathon_id: 1
         }, {
           code: 'stu',
           expires_on: '2018-03-01',
-          project_id: null,
+          student_id: null,
           hackathon_id: 1
         }, {
           code: 'vwx',
           expires_on: '2018-10-01',
-          project_id: null,
+          student_id: null,
           hackathon_id: 2
         }, {
           code: 'yz0',
           expires_on: '2018-10-01',
-          project_id: null,
+          student_id: null,
           hackathon_id: 2
         }
       ])
