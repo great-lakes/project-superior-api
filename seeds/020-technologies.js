9b2c3f1312235a7090fdecf21eb4f64cfaa6bd74
Hao Luo <howlowck@gmail.com>
2018-02-11 22:30:58 -0600

    deleted a space

--- seeds/020-technologies.js
+++ seeds/020-technologies.js
@@ -11,5 +11,5 @@ exports.seed = (knex, Promise) =>
         {name: 'Azure Container Services'},
         {name: 'Azure Iothub'},
         {name: 'CNTK'},
-        {name: 'Bot Framework', doc_link: 'https://dev.botframework.gettingStarted.example', help_text: ' Bots are a great way to create artificial human interaction with your users. Combine bots with Cognitive Services and you will be able to create an intelligent chatbot capable of understanding language intentions and much more.'}
+        {name: 'Bot Framework', doc_link: 'https://dev.botframework.gettingStarted.example', help_text: 'Bots are a great way to create artificial human interaction with your users. Combine bots with Cognitive Services and you will be able to create an intelligent chatbot capable of understanding language intentions and much more.'}
       ])
