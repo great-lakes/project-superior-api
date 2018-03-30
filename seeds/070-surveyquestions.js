f0992f387c74fd7e1121d99dab69ea92f33942e9
Hao Luo <howlowck@gmail.com>
2018-03-30 11:00:57 -0500

    changed data seed for survey questions and choices

--- seeds/070-surveyquestions.js
+++ seeds/070-surveyquestions.js
@@ -2,19 +2,24 @@ const dbName = 'surveyquestions'
 exports.seed = (knex, Promise) =>
   knex(dbName).insert([
     {
-      prompt: 'What is your first name?',
-      type: 'TEXT',
+      prompt: 'What technology do you want to work on? (select one)',
+      type: 'CHOICE',
       survey_id: 1,
       order: 1
     }, {
-      prompt: 'What is your last name?',
-      type: 'TEXT',
+      prompt: 'What language do you want to work on? (select one)',
+      type: 'CHOICE',
       survey_id: 1,
       order: 2
     }, {
-      prompt: 'What is your major?',
+      prompt: 'What company do you want to work for the most? (select one)',
       type: 'CHOICE',
       survey_id: 1,
       order: 3
+    }, {
+      prompt: 'Can you tell me a little more about your project?',
+      type: 'TEXT',
+      survey_id: 1,
+      order: 4
     }
   ])
--- seeds/075-surveychoices.js
+++ seeds/075-surveychoices.js
@@ -2,28 +2,64 @@ const dbName = 'surveychoices'
 exports.seed = (knex, Promise) =>
   knex(dbName).insert([
     {
+      surveyquestion_id: 1,
+      value: 'Machine Learning',
+      order: 1
+    }, {
+      surveyquestion_id: 1,
+      value: 'Computer Vision',
+      order: 2
+    }, {
+      surveyquestion_id: 1,
+      value: 'Web Service',
+      order: 3
+    }, {
+      surveyquestion_id: 1,
+      value: 'Big Data',
+      order: 4
+    }, {
+      surveyquestion_id: 1,
+      value: 'Chat Bot',
+      order: 5
+    }, {
+      surveyquestion_id: 2,
+      value: 'Python',
+      order: 1
+    }, {
+      surveyquestion_id: 2,
+      value: 'Javascript',
+      order: 2
+    }, {
+      surveyquestion_id: 2,
+      value: 'C++',
+      order: 3
+    }, {
+      surveyquestion_id: 2,
+      value: 'C#',
+      order: 4
+    }, {
+      surveyquestion_id: 2,
+      value: 'Scala',
+      order: 5
+    }, {
+      surveyquestion_id: 2,
+      value: 'Java',
+      order: 6
+    }, {
       surveyquestion_id: 3,
-      value: 'CE',
+      value: 'Amazon',
       order: 1
     }, {
       surveyquestion_id: 3,
-      value: 'EE',
+      value: 'Microsoft',
       order: 2
     }, {
       surveyquestion_id: 3,
-      value: 'CS',
+      value: 'Facebook',
       order: 3
     }, {
       surveyquestion_id: 3,
-      value: 'ME',
+      value: 'Google',
       order: 4
-    }, {
-      surveyquestion_id: 3,
-      value: 'Biology',
-      order: 5
-    }, {
-      surveyquestion_id: 3,
-      value: 'Other',
-      order: 6
     }
   ])
